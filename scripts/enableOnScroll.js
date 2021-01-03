d2a6f4f180ac6a84868c0784fa811864446c922e
Tiago Dinis <tiagoddinis@gmail.com>
2021-01-03 16:57:27 +0000

    homepage and refactoring

--- scripts/enableOnScroll.js
+++ scripts/enableOnScroll.js
@@ -6,25 +6,6 @@ function updateTopNav() {
 
 window.addEventListener("scroll", debounce(updateTopNav, 5));
 
-// -------------------------------------------------------------------------------------------------
-const song = document.getElementById("song");
-const playButton = document.getElementById("play-button");
-var isPlaying = false;
-
-function toggleSong() {
-	if (isPlaying){
-		song.pause();
-		playButton.src = "../images/play.svg"
-	}
-	else{
-		song.play();
-		playButton.src = "../images/pause.svg"
-	}
-	isPlaying = !isPlaying;
-}
-
-if (playButton) playButton.addEventListener("click", toggleSong, false);
-
 // -- Utils ----------------------------------------------------------------------------------------
 
 // Call "func" after it stops being called for "ms" milliseconds
---
+++ scripts/playSong.js
@@ -0,0 +1,17 @@
+const song = document.getElementById("song");
+const playButton = document.getElementById("play-button");
+var isPlaying = false;
+
+function toggleSong() {
+	if (isPlaying){
+		song.pause();
+		playButton.src = "../images/play.svg"
+	}
+	else{
+		song.play();
+		playButton.src = "../images/pause.svg"
+	}
+	isPlaying = !isPlaying;
+}
+
+if (playButton) playButton.addEventListener("click", toggleSong, false);
